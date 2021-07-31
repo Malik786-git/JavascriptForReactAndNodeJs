ef793a37f93f05bf66c55ee943a88d8b451a7d4c
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2021-08-01 03:46:31 +0500

    update

---
+++ DOM/DOM.js
@@ -0,0 +1,9 @@
+////////////////////// BOM //////////////////////////////////
+
+// window.history.back(); // use to check last url visit
+
+// location.href() // use to check url of current website also assign new link to move another
+// location.href = "facebook.com";
+
+////////////////////// DOM ///////////////////////////////////
+
--- tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-; 
-// console.log(Math.ceil(9.5)); 
-// console.log(Math.ceil(1.9)); 
